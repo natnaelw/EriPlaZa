52ea4d65de48e0a82459e6d5253fd31484456bbc
teklefilmon <teklefilmon@yahoo.com>
2015-06-14 15:14:27 -0500

    update PaymentController adding form backing bean

--- src/main/java/com/mum/eriplaza/controller/PaymentController.java
+++ src/main/java/com/mum/eriplaza/controller/PaymentController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mum.eriplaza.domain.CreditCard;
+
 @Controller
 public class PaymentController {
 	
 	@RequestMapping("/payment")
 	public String payment(Model model){
-		System.out.println("payment processing");
+		CreditCard creditCard = new CreditCard();
+		model.addAttribute("creditCard", creditCard);
 		return "payment";
 	}
 }
