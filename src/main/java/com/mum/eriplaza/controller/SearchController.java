6d8bf13209fd05e1cb0e6be26eb6b443060276e5
Natnael Welday <natnael.welday@gmail.com>
2015-06-15 14:01:52 -0500

    + Search Controller modification

--- src/main/java/com/mum/eriplaza/controller/SearchController.java
+++ src/main/java/com/mum/eriplaza/controller/SearchController.java
@@ -1,16 +1,11 @@
 package com.mum.eriplaza.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.mum.eriplaza.domain.Category;
-import com.mum.eriplaza.domain.Item;
 import com.mum.eriplaza.services.CategoryService;
 import com.mum.eriplaza.services.ItemService;
 
@@ -30,22 +25,15 @@ public class SearchController {
 	
 	@RequestMapping("/search")
 	public String search(Model model){
-		List<Category> categories = categoryService.findAll();
-		for (Category category : categories) {
-			System.out.println("---*"+category);
-		}
-		model.addAttribute("categories", categories);
+		
+		model.addAttribute("categories", categoryService.findAll());
 		return "search";
 	}
 	
 	@RequestMapping(value ="/searchByCategory")
 	public String getProductsByCategory(Model model,@RequestParam("categoryId")String categoryId) {
-
-		List<Item> items = itemService.findItemsByCategory(Long.parseLong(categoryId));
-        for (Item item : items) {
-			System.out.println("--"+item);
-		}
-		model.addAttribute("items", items);
+		
+		model.addAttribute("items", itemService.findItemsByCategory(Long.parseLong(categoryId)));
 		return "items";
 	}
 
